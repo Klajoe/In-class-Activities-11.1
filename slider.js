3ba5104e4bf9726229c0f4a3239f0f42f60f1fb8
Klajoe <parlar_ar@hotmail.com>
2023-05-30 20:18:47 +0300

    ..

--- slider.js
+++ slider.js
@@ -1,25 +1,14 @@
-$(document).ready(function() {
-    $("#slider").bxSlider({
-        auto: true,
-        randomStart: true,
-        moveSlides: 1,
-        minSlides: 1,
-        maxSlides: 1,
-        slideWidth: 250,
-        slideMargin: 10,
-        pager: true,
-        pagerCustom: '#pager',
-        pause: 3000,
-        onSliderLoad: function(currentIndex) {
-            var currentSlide = $("#slider li").eq(currentIndex);
-            var caption = currentSlide.find('.caption').text();
-            var pagerIcon = currentSlide.find('.pager-icon').html();
-            $("#pager").html(caption + '<br>' + pagerIcon);
-        },
-        onSlideAfter: function($slideElement, oldIndex, newIndex) {
-            var caption = $slideElement.find('.caption').text();
-            var pagerIcon = $slideElement.find('.pager-icon').html();
-            $("#pager").html(caption + '<br>' + pagerIcon);
-        }
-    });
+$(document).ready(function () {
+	$("#slider").bxSlider({
+		auto: true,
+		slideWidth: 250,
+		slideMargin: 10,
+		randomStart: true,
+		speed: 300,
+		moveSlides: 1,
+		pager: true,
+		pagerType: "short",
+		touchEnabled: false
+	});
+
 });
